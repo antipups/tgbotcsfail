7183cb17763455ce71fb00678e5f1616888d91c9
antipups <51057725+antipups@users.noreply.github.com>
2020-10-26 00:44:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '1318152632:AAEXJmbomT143xDilK_kGiegt1_-qWxY9lU'    # что за бот
+TOKEN = ''    # что за бот
 
 # для получения данных по апи
 headers = {'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
@@ -9,4 +9,4 @@ headers = {'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
            'sec-fetch-site': 'same-site',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',}
 
-orderer = '1259686452'  # ид куда идёт информация
+orderer =   # ид куда идёт информация
